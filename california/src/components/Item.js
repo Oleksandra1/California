ab49ccc76d017e36956767dcbde6098d3b51fb0c
Oleksandra1 <oleksandra.tkachenko154.com>
2024-08-31 14:23:28 +0300

    react_project

--- california/src/components/Item.js
+++ california/src/components/Item.js
@@ -5,7 +5,7 @@ export class Item extends Component {
   render() {
     return (
       <div className='item'>
-        <img src={"./goods/" + this.props.item.img} onClick={() => this.props.onShowItem()} />
+        <img src={"./goods/" + this.props.item.img} onClick={() => this.props.onShowItem(this.props.item)} />
         <h2>{this.props.item.title}</h2>
         <b>{this.props.item.price}$</b>
         <div className='add-to-cart' onClick={() => this.props.onAdd(this.props.item)}>+</div>
--- california/src/components/ShowFullItem.js
+++ california/src/components/ShowFullItem.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react'
 export class ShowFullItem extends Component {
   render() {
     return (
-      <div>ShowFullItem</div>
+      <div className='full-item'>
+        <div className=''>
+          <img src={"./goods/" + this.props.item.img} onClick={() => this.props.onShowItem(this.props.item)} />
+          <h2>{this.props.item.title}</h2>
+          <b>{this.props.item.price}$</b>
+          <div className='add-to-cart' onClick={() => this.props.onAdd(this.props.item)}>+</div>
+        </div>      
+      </div>
     )
   }
 }
